b5fa17bdd0ddd3d5eee571b5e936e20a3792a522
Luiz Augusto <118451560+lu1zcarvalho@users.noreply.github.com>
2023-05-28 13:31:40 -0300

    Add files via upload

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Episodio ep1 = new(2,"Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flávia");
+
+
+Podcast podcast = new("Podcast especial", "Luiz");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExbirDetalhes();
